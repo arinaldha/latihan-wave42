5b0b38f3536849842dbf2cafb5c7cc21a976133a
Arinal Dzikrul <arinal.dzikrul@sinarmasmining.com>
2023-12-21 21:15:27 +0700

    fix latihan manipulasi text

--- day5/variable.js
+++ day5/variable.js
@@ -1,4 +1,5 @@
 let namaDepan = "Arinal";
 let namaBelakang = "Dzikrul";
 let result = `Halo nama saya ${namaDepan} ${namaBelakang} saya sedang belajar javascript`;
-console.log(result.replace("saya", "aku"));
+const regex = new RegExp("saya", "g");
+console.log(result.replace(regex, "aku"));
