e81c905f0ae3a73aee4743223fad399188422b7b
Arinal Dzikrul <arinal.dzikrul@sinarmasmining.com>
2023-12-21 21:12:24 +0700

    latihan manipulasi text

---
+++ day5/variable.js
@@ -0,0 +1,4 @@
+let namaDepan = "Arinal";
+let namaBelakang = "Dzikrul";
+let result = `Halo nama saya ${namaDepan} ${namaBelakang} saya sedang belajar javascript`;
+console.log(result.replace("saya", "aku"));
